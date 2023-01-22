7f562ae4ec775178d82fb5e4f1ae840164006d0e
ephraim <mekgwele@gmail.com>
2023-01-22 13:10:19 +0200

    Basic html
    Add basic HTML for ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// import { FeatherModule } from 'angular-feather';
+// import { allIcons } from 'angular-feather/icons';
 import { AppComponent } from './app.component';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FeatherModule.pick(allIcons)
+  ],
+  exports: [
+    // FeatherModule
   ],
   providers: [],
   bootstrap: [AppComponent]
