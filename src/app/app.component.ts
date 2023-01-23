21e31107ae68fddd8bd746bd2f2de11197fabde4
ephraim <mekgwele@gmail.com>
2023-01-23 03:01:01 +0200

    update UI
    Give the UI a makeover

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,8 @@ export class AppComponent {
 
   public temp_unit = "C";
   public location = "Johannesburg";
+  public weather_msg = "";
+  public weather_bg_image = "";
   
   constructor(
     private _weather_data_service: WeatherDataService,
@@ -62,16 +64,16 @@ export class AppComponent {
     this.selected_weather = this.weather_data[0];
 
 
-    this.viewDayWeather(0);
+    this.viewTimeWeather(0, 'day');;
 
-    // console.log(this.data_sample);
-    // console.log(this.weather_data);
-    // console.log(this.selected_location);
-    // console.log(this.selected_weather);
+    console.log(this.data_sample);
+    console.log(this.weather_data);
+    console.log(this.selected_location);
+    console.log(this.selected_weather);
 
-    // this.weather_data.forEach((element:any) => {
-    //     console.log(element);
-    // });
+    this.weather_data.forEach((element:any) => {
+        console.log(element);
+    });
 
     /**
      * END
@@ -79,8 +81,6 @@ export class AppComponent {
 
   };
 
-
-  
 
   // Group data by date (Array by key)
   groupBy:any = (array:any, key:any) => {
@@ -98,24 +98,54 @@ export class AppComponent {
   };
 
 
-  viewDayWeather(index:any){
+  // When clicking to view specific time
+  viewTimeWeather(index:any, type:any){
 
-    this.selected_weather = this.weather_data[index];   
-    this.weather_summary = {
-      'temp':`${this.convertTemparature(this.selected_weather[0].main.temp)} °${this.temp_unit}`,
-      'date': this.selected_weather[0].date,
-      'time': this.selected_weather[0].time,
-      'weekday': this.selected_weather[0].weekday,
-      'humidity': this.selected_weather[0].main.humidity,
-      'wind': `${this.selected_weather[0].wind.speed} km\h`,
-      'location': this.location,
-      'desc': this.selected_weather[0].weather[0].description,
+    if(type == "day"){
+      this.selected_weather = this.weather_data[index];
+      index = 0;
     }
-  }
 
-  viewTimeWeather(index:any){
+    // SET background images based in different conditions
+    if(this.selected_weather[index].weather[0].main == "Clear"){
+      this.weather_bg_image = 'url(../assets/clear.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Drizzle"){
+      this.weather_bg_image = 'url(../assets/drizzle.jpeg)';
+    }else if(this.selected_weather[index].weather[0].main == "Rain"){
+      this.weather_bg_image = 'url(../assets/rain.jpeg)';
+    }else if(this.selected_weather[index].weather[0].main == "Clouds"){
+      this.weather_bg_image = 'url(../assets/clouds.jpeg)';
+    }else if(this.selected_weather[index].weather[0].main == "Thunderstorm"){
+      this.weather_bg_image = 'url(../assets/thunder.jpeg)';
+    }else if(this.selected_weather[index].weather[0].main == "Snow"){
+      this.weather_bg_image = 'url(../assets/snow.webp)';
+    }else if(this.selected_weather[index].weather[0].main == "Mist"){
+      this.weather_bg_image = 'url(../assets/mist.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Smoke"){
+      this.weather_bg_image = 'url(../assets/smoke.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Haze"){
+      this.weather_bg_image = 'url(../assets/haze.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Dust"){
+      this.weather_bg_image = 'url(../assets/dust.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Fog"){
+      this.weather_bg_image = 'url(../assets/fog.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Sand"){
+      this.weather_bg_image = 'url(../assets/sand.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Dust"){
+      this.weather_bg_image = 'url(../assets/dust.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Ash"){
+      this.weather_bg_image = 'url(../assets/ash.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Squall"){
+      this.weather_bg_image = 'url(../assets/squall.jpg)';
+    }else if(this.selected_weather[index].weather[0].main == "Tornado"){
+      this.weather_bg_image = 'url(../assets/tornado.jpg)';
+    }else{
+      this.weather_bg_image = '';
+    }
+
     this.weather_summary = {
       'temp': `${this.convertTemparature(this.selected_weather[index].main.temp)} °${this.temp_unit}`,
+      'temp_kelvin':this.selected_weather[index].main.temp,
       'date': this.selected_weather[index].date,
       'time': this.selected_weather[index].time,
       'weekday': this.selected_weather[index].weekday,
@@ -123,6 +153,7 @@ export class AppComponent {
       'wind': `${this.selected_weather[index].wind.speed} km\h`,
       'location': this.location,
       'desc': this.selected_weather[index].weather[0].description,
+      'icon': `http://openweathermap.org/img/wn/${this.selected_weather[index].weather[0].icon}@2x.png`,
     }
   }
 
@@ -132,6 +163,17 @@ export class AppComponent {
 
     if(this.temp_unit == "C"){
       temperature = kelvinVal - 273.15;
+
+      if(temperature < 15){
+        this.weather_msg = "It's about to get cold, maybe you should grap a jacket.";
+      }else if(temperature > 25){
+        this.weather_msg = "It's about to get really hot, remember to drink lots of water.";
+      }else{
+        this.weather_msg = "";
+      }
+
+      console.log(temperature);
+      
     }else if(this.temp_unit == "F"){
       temperature = (kelvinVal-273.15)*9/5+32;
     }else{
@@ -139,6 +181,17 @@ export class AppComponent {
     }
 
     return temperature.toFixed(1);
+  }
+
+  // Switch temparature unit
+  switchTempUnit(unit:any){
+    if(unit == "C"){
+      this.temp_unit = "C";
+    }else if(unit == "F"){
+      this.temp_unit = "F";
+    }
+    
+    this.weather_summary.temp = `${this.convertTemparature(this.weather_summary.temp_kelvin)} °${this.temp_unit}`;
   }
 
 
@@ -170,7 +223,9 @@ export class AppComponent {
       this.weather_data = Object.values(this.weather_data);
 
       // Select the first weather and set as selected
-      this.selected_weather = this.weather_data[0][0];      
+      this.selected_weather = this.weather_data[0][0];    
+      
+      this.viewTimeWeather(0, 'day');
 
       },error: error => {
 
